a29db963110223b3408a065ddf3ffdd264d50c46
Himanshu-0711 <himanshuwagh23@gmail.com>
2024-07-12 13:01:16 +0530

    Create after-install.sh

---
+++ after-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Reload Nginx to apply changes
+systemctl reload nginx
+
+# You can add additional post-deployment actions here if needed
+# For example, restarting services, setting permissions, etc.
